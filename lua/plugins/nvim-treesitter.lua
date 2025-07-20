cc903ef9cbb0b51d4f8805073f1b6c09dfed14c2
Sturmtiger <maksim155555@gmail.com>
2025-07-20 22:27:33 +0300

    Configure treesitter [in progress].

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,20 +1,30 @@
+-- TODO: Configure folding
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  main = 'nvim-treesitter.configs',
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
+    ensure_installed = {
+			'python',
+	-- The following parsers MUST always be installed as to nvim-treesitter docs.
+	'c',
+	'lua',
+	'vim',
+	'vimdoc',
+	'query',
+	'markdown',
+	'markdown_inline',
+		},
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
     highlight = {
       enable = true,
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
+      --  the list of `additional_vim_regex_highlighting` and disabled languages for indent (`indent.disable` table).
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = { enable = true },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
