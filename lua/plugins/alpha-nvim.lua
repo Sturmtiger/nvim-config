58a2b867c2eb7df08420fe7e920b9e37fe798105
Sturmtiger <maksim155555@gmail.com>
2025-10-01 19:40:27 +0300

    Configure alpha-nvim.

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -1,12 +1,16 @@
 return {
   'goolord/alpha-nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
   lazy = false,
   config = function()
-    local startify = require 'alpha.themes.startify'
-    startify.file_icons.provider = 'devicons'
-    require('alpha').setup(startify.config)
+    local dashboard = require 'alpha.themes.dashboard'
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  New file', '<cmd>enew<CR>'),
+      dashboard.button('r', '  Recently used files', '<cmd>Telescope oldfiles<CR>'),
+      dashboard.button('f', '󰈞  Find file', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('s', '󱙋  Manage sessions', '<cmd>Telescope persisted<CR>'),
+      dashboard.button('c', '  Update plugins', '<cmd>Lazy sync<CR>'),
+      dashboard.button('q', '  Quit', '<cmd>qa<CR>'),
+    }
+    require('alpha').setup(dashboard.config)
   end,
 }
