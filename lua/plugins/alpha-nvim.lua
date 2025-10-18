93b73505fbcb0c0c5e51a44b2a1f0e780d264897
Sturmtiger <maksim155555@gmail.com>
2025-10-18 21:27:10 +0300

    Fixes.

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -7,8 +7,8 @@ return {
       dashboard.button('e', '  New file', '<cmd>enew<CR>'),
       dashboard.button('r', '  Recently used files', '<cmd>Telescope oldfiles<CR>'),
       dashboard.button('f', '󰈞  Find file', '<cmd>Telescope find_files<CR>'),
-      dashboard.button('s', '󱙋  Manage sessions', '<cmd>Telescope persisted<CR>'),
-      dashboard.button('c', '  Update plugins', '<cmd>Lazy sync<CR>'),
+      dashboard.button('s', '  Manage sessions', '<cmd>Telescope persisted<CR>'),
+      dashboard.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
       dashboard.button('q', '  Quit', '<cmd>qa<CR>'),
     }
     require('alpha').setup(dashboard.config)
