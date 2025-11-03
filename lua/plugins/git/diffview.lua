287cdf9c373632cb1a26e29887ae09a80736681d
Sturmtiger <maksim155555@gmail.com>
2025-11-03 21:23:57 +0200

    Configure diffview [in progress].

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,3 +1,12 @@
 return {
   'sindrets/diffview.nvim',
+  keys = {
+    { '<leader>gf', '<cmd>DiffviewFileHistory<CR>', desc = 'File history' },
+    { '<leader>gd', '<cmd>DiffviewOpen<CR>', desc = 'Diff view' },
+  },
+  opts = {
+    default_args = {
+      DiffviewFileHistory = { '%' },
+    },
+  },
 }
