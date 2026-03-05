d17e10817ff6d811cd18b4be29bfcf6999f56b77
Sturmtiger <maksim155555@gmail.com>
2026-03-05 23:01:45 +0200

    Remove nvim-cmp.

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -7,9 +7,6 @@ return {
 
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
-
-    -- Allows extra capabilities provided by nvim-cmp
-    'hrsh7th/cmp-nvim-lsp',
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -40,18 +37,14 @@ return {
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
         keymap('grt', require('telescope.builtin').lsp_type_definitions, 'Go to type definition')
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        keymap('K', vim.lsp.buf.hover, 'Hover element under cursor')
 
         local lsp_methods = vim.lsp.protocol.Methods
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client:supports_method(lsp_methods.textDocument_documentHighlight, event.buf) then
           local highlight_augroup = vim.api.nvim_create_augroup('sturm/lsp_highlight', { clear = false })
+
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
@@ -65,7 +58,7 @@ return {
           })
 
           vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            group = vim.api.nvim_create_augroup('sturm/lsp_detach', { clear = true }),
             callback = function(event2)
               vim.lsp.buf.clear_references()
               vim.api.nvim_clear_autocmds { group = 'sturm/lsp_highlight', buffer = event2.buf }
@@ -73,10 +66,6 @@ return {
           })
         end
 
-        -- The following code creates a keymap to toggle inlay hints in your
-        -- code, if the language server you are using supports them
-        --
-        -- This may be unwanted, since they displace some of your code
         if client and client:supports_method(lsp_methods.textDocument_inlayHint, event.buf) then
           keymap('<leader>th', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
@@ -85,7 +74,6 @@ return {
       end,
     })
 
-    -- Diagnostic Config
     -- See :help vim.diagnostic.Opts
     vim.diagnostic.config {
       severity_sort = true,
@@ -119,7 +107,6 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
